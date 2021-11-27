ed82249982bfcf0d49cd55688e00f943f95496d6
Pavan Kumar Uppuluri <63893920+pavanuppuluri@users.noreply.github.com>
2021-11-27 18:26:38 +0530

    React Redux State Management

---
+++ src/components/Counter.js
@@ -0,0 +1,30 @@
+import { useSelector, useDispatch } from 'react-redux';
+
+import classes from './Counter.module.css';
+
+const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+
+  const incrementHandler = () => {
+    dispatch({ type: 'increment' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'decrement' });
+  };
+
+  
+  return (
+    <main className={classes.counter}>
+      <b style={{ color:"blue"}}>Redux State Management</b>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>      
+    </main>
+  );
+};
+
+export default Counter;
